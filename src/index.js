7543a7bdcb1d7de05792292bb32e71bc22c448b0
Rezvanova Daria <daria.rezv@gmail.com>
2020-04-22 01:58:20 +0600

    Added store

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from "react-router-dom";
-import { createStore, applyMiddleware } from 'redux';
 import {Provider} from 'react-redux'
-import thunk from 'redux-thunk';
 
 
 import Base from './layouts/base/Base';
 import Todo from './pages/Todo/Todo';
 import Done from './pages/Done/Done';
+import store from './store/store'
 
 import './index.css';
 
-export const newStore = () => {
-    return createStore(
-        ()=>{},
-        applyMiddleware(thunk),
-    );
-};
 
-const store = createStore(() => {
-});
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+    ()=>{},
+    applyMiddleware(thunk),
+);
+
+export default store;
